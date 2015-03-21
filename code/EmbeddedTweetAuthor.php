8b0d7b1df0c442c77a827cef0141efb01fbfea97
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-21 17:03:56 +0700

    FIX: Whitespace

--- code/EmbeddedTweetAuthor.php
+++ code/EmbeddedTweetAuthor.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class EmbeddedTweetAuthor extends DataObject {
 	private static $db = array(
@@ -12,9 +12,9 @@ class EmbeddedTweetAuthor extends DataObject {
 
 }
 /*
-Array ( [cache_age] => 3153600000 [url] => https://twitter.com/qandrew/statuses/469515779415625729 
-[height] => [provider_url] => https://twitter.com [provider_name] => Twitter 
-[author_name] => Andrew Clark 
-[version] => 1.0 
+Array ( [cache_age] => 3153600000 [url] => https://twitter.com/qandrew/statuses/469515779415625729
+[height] => [provider_url] => https://twitter.com [provider_name] => Twitter
+[author_name] => Andrew Clark
+[version] => 1.0
 [author_url] => https://twitter.com/qandrew [type] => rich [html] =>
 */
