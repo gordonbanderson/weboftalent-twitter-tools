6fd781fa33dfca1e50070d1bee5031bf37b39c59
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-20 11:54:56 +0700

    MINOR: Remove debug tweet info

--- code/EmbeddedTweet.php
+++ code/EmbeddedTweet.php
@@ -16,10 +16,3 @@ class EmbeddedTweet extends DataObject {
         'TwitterID' => true
     );
 }
-/*
-Array ( [cache_age] => 3153600000 [url] => https://twitter.com/qandrew/statuses/469515779415625729
-[height] => [provider_url] => https://twitter.com [provider_name] => Twitter
-[author_name] => Andrew Clark
-[version] => 1.0
-[author_url] => https://twitter.com/qandrew [type] => rich [html] =>
-*/
--- code/EmbeddedTweetAuthor.php
+++ code/EmbeddedTweetAuthor.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class EmbeddedTweetAuthor extends DataObject {
 	private static $db = array(
@@ -11,10 +11,3 @@ class EmbeddedTweetAuthor extends DataObject {
 	);
 
 }
-/*
-Array ( [cache_age] => 3153600000 [url] => https://twitter.com/qandrew/statuses/469515779415625729 
-[height] => [provider_url] => https://twitter.com [provider_name] => Twitter 
-[author_name] => Andrew Clark 
-[version] => 1.0 
-[author_url] => https://twitter.com/qandrew [type] => rich [html] =>
-*/
