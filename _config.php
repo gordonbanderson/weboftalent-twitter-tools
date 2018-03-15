f8250bf9a0cbaefbd57300ca54a77a91c6c089cb
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-15 21:58:32 +0700

    MINOR: Remove debug

--- _config.php
+++ _config.php
@@ -1,6 +1,3 @@
 <?php
 use SilverStripe\View\Parsers\ShortcodeParser;
-
-echo '**** TWITTER SC SETUP MODULE ****';
 ShortcodeParser::get('default')->register('tweet',[WebOfTalent\TwitterTools\TwitterShortCodeHandler::class,'handle_shortcode']);
-echo '**** /TWITTER SC SETUP MODULE ****';
