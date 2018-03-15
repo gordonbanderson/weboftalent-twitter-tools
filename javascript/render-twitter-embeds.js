6bdf108de0b6328aa916b8c84478b8f5d5bb5425
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-15 23:06:52 +0700

    WIP: Rendering via a mix of create tweet and twitter markup

---
+++ javascript/render-twitter-embeds.js
@@ -0,0 +1,36 @@
+window.onload = (function(){
+
+
+});
+
+twttr.ready(function (twttr) {
+    console.log('Twitter ready');
+
+    var tweets = document.getElementsByClassName("tweet");
+    var i;
+    for (i = 0; i < tweets.length; i++) {
+        var tweet = tweets[i];
+        var id = tweet.getAttribute("tweetID");
+        console.log('Loading ' , id);
+        twttr.widgets.createTweet(
+            id, tweet,
+            {
+                conversation : 'none',    // or all
+                cards        : 'visible',  // or visible
+                linkColor    : '#cc0000', // default is blue
+                theme        : 'light',    // or dark,
+                'align'		 : 'center'
+            });
+    }
+    
+
+    twttr.events.bind(
+        'loaded',
+        function (event) {
+            console.log('Loaded');
+            event.widgets.forEach(function (widget) {
+                console.log("Created widget", widget.id);
+            });
+        }
+    );
+});
